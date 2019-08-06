f03462cbc733ca8a062291bddebd372b674e9662
C Vamshi Krishna <53230977+insaiyancvk@users.noreply.github.com>
2019-08-06 12:40:41 +0530

    Update TooLong.py
    Updated the ranges of number of inputs and length of strings

--- Code Forces/TooLong.py
+++ Code Forces/TooLong.py
@@ -1,17 +1,24 @@
 n = int(input("Enter the number of words you want to enter : "))
-w = []
-l = []
-e = ''
-print("Enter the words : ")
-for i in range(0,n):
-    w.append(input())
+if(n<1 and n>100):
+   print("invalid input") 
+else:
+   w = []
+   l = []
+   e = ''
+   print("Enter the words : ")
+   for i in range(0,n):
+      w.append(input())
 
-print("The abbriviation : ")
-for j in range(n):
-    e = w[j]
-    l = len(w[j])
-    if(l>10):
-        print(e[0]+str(l-2)+e[l-1])
-    else:
-        print(w[j])
+   print("The abbriviation : ")
+   for j in range(n):
+      e = w[j]
+      l = len(w[j])
+      if(l<1 and l>100):
+         print("invalid input") 
+         break
+      else:
+         if(l>10):
+             print(e[0]+str(l-2)+e[l-1])
+         else:
+             print(w[j])
     
