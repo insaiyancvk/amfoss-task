c459abae72c831a501abc890af9eb632b6df3db9
C Vamshi Krishna <53230977+insaiyancvk@users.noreply.github.com>
2019-08-06 18:39:34 +0530

    Update TheatreSquare.py

--- Code Forces/TheatreSquare.py
+++ Code Forces/TheatreSquare.py
@@ -8,7 +8,7 @@ a=float(input("Enter length of the flagstone : "))
 
 t = (math.ceil(n/a))*(math.ceil(m/a))
 
-if((n<1 or n>(math.pow(10,9))) and (m<1 or m>(math.pow(10,9))) and (a<1 or a>(math.pow(10,9)))):
+if((n<1 or n>(math.pow(10,9))) or (m<1 or m>(math.pow(10,9))) or (a<1 or a>(math.pow(10,9)))):
     print("Invalid input") 
 else:
     print(t)
