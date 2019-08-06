e4a03051caf8c1624f098a1bf7cacdb83eed218d
C Vamshi Krishna <53230977+insaiyancvk@users.noreply.github.com>
2019-08-06 09:02:05 +0530

    Update BdayCase.py

--- Hackerrank/BDayCake.py
+++ Hackerrank/BDayCake.py
@@ -3,16 +3,20 @@ n=int(input("Enter the number of candles : "))
 
 c = 0
 print("Enter the size of them : ")
-for i in range(n):
-    m = []
-    m.append(int(input()))
-    a.append(m)
-    
-g = max(a)
+if(n<1 or n>100000):
+    print ("Invalid input") 
+else:
 
-for i in range(n):
-    if(a[i]==g):
-        c += 1
+    for i in range(n):
+        m = []
+        m.append(int(input()))
+        a.append(m)
+    
+    g = max(a)
+
+    for i in range(n):
+        if(a[i]==g):
+            c += 1
 
 print("The number of tallest candles are : ",c)
 
