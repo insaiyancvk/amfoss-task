0a499cfae56faf79871c3c91db1f86a0a8bef9d7
C Vamshi Krishna <53230977+insaiyancvk@users.noreply.github.com>
2019-08-16 23:14:55 +0530

    Add files via upload

---
+++ rustedMail.rs
@@ -0,0 +1,34 @@
+extern crate regex;
+
+use regex::Regex;
+
+fn main()
+	 {
+    use std::io::{stdin,stdout,Write};
+
+
+    let mut mail=String::new();
+
+
+    print!("Enter the E Mail to be checked : ");
+
+
+    let _=stdout().flush();
+
+
+    stdin().read_line(&mut mail).expect("error: unable to read user input");
+
+
+	if Regex::new(r"^[a-zA-Z0-9]+@[a-z]{2,8}[.]?[a-z]*").unwrap().is_match(&mut mail) == true
+	{
+
+	    println!("The E-Mail entered is valid");
+
+	}
+	else
+	{
+
+	    println!("The entered E Mail is not valid");
+
+	}
+}
