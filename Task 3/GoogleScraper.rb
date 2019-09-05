0d9305a1f79b1eb58ad6dc30e6a8c26cf8d9e497
C Vamshi Krishna <53230977+insaiyancvk@users.noreply.github.com>
2019-09-05 23:09:39 +0530

    Update GoogleScraper.rb

--- Task 3/GoogleScraper.rb
+++ Task 3/GoogleScraper.rb
@@ -1,9 +1,18 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require 'ruby-progressbar'
 
 puts "Enter a search phrase"
 search = gets
+
+array = Array.new(1001)
+progressbar = ProgressBar.create(:total => array.size)
+
+array.each do |item|
+    progressbar.increment
+    sleep 0.005
+end
 
 doc = Nokogiri::HTML(open('https://www.google.com/search?num=11&q='+search))
 
